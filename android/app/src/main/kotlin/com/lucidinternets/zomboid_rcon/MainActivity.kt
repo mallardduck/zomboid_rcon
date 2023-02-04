b09e40c0ef05224449f9cb5507503d70d98532fc
Dan Pock <self@danpock.me>
2023-02-04 12:49:41 -0500

    init project

---
+++ android/app/src/main/kotlin/com/lucidinternets/zomboid_rcon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lucidinternets.zomboid_rcon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:zomboid_rcon/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
