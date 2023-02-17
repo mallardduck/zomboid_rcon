4b208f07d9bde1a51074bc1595ac18b78b383282
Dan Pock <self@danpock.me>
2023-02-17 10:45:10 -0500

    Fix minimum window size on desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zomboid_rcon/app.dart';
-import 'package:zomboid_rcon/database.dart';
+import 'package:window_size/window_size.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowMinSize(const Size(450, 400));
+    setWindowMaxSize(Size.infinite);
+  }
 
   runApp(
       const ProviderScope(
