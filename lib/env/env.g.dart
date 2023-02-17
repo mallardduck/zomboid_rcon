048777d9687230ac35dea895abf2e3251e2b4e5b
Dan Pock <self@danpock.me>
2023-02-17 11:51:00 -0500

    add way to use (compile time) Env vars in App

---
+++ lib/env/env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const dbSecret = 'my_secure_db_secret';
+}
